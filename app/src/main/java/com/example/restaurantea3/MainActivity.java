dc58167ffa861812f0d1f03f9f4826b103aee114
Rodrigo Lopes <rodrigo.bleachex@hotmail.com>
2022-09-13 09:36:14 -0300

    first commit

---
+++ app/src/main/java/com/example/restaurantea3/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.restaurantea3;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.restaurantea3.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/com/example/restaurantea3/activity_main.java
@@ -0,0 +1,4 @@
+package com.example.restaurantea3;
+
+public class activity_main {
+}
