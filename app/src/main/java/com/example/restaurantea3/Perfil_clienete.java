2a0e7a3e6d0401b9de68b93e423d6fb747ad799f
Rodrigo Lopes <rodrigo.bleachex@hotmail.com>
2022-09-17 12:37:44 -0300

    modal qr code

--- app/src/main/java/com/example/restaurantea3/Perfil_clienete.java
+++ app/src/main/java/com/example/restaurantea3/Perfil_clienete.java
@@ -1,9 +1,11 @@
 package com.example.restaurantea3;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageButton;
 
 public class Perfil_clienete extends AppCompatActivity {
@@ -15,6 +17,7 @@ public class Perfil_clienete extends AppCompatActivity {
 
         ImageButton bt_return_home = (ImageButton) findViewById(R.id.bt_return_home);
         ImageButton bt_logout = (ImageButton) findViewById(R.id.bt_logout);
+        ImageButton open_qr_cliente = (ImageButton) findViewById(R.id.open_qr_cliente);
 
         bt_return_home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +34,18 @@ public class Perfil_clienete extends AppCompatActivity {
                 Intent intent = new Intent();
                 intent.setClass(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        open_qr_cliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(Perfil_clienete.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setCancelable(true);
+                dialog.setContentView(R.layout.modal_qr_code);
+
+                dialog.show();
             }
         });
     }
