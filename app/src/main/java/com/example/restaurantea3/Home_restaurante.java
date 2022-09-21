8967741a5e303f3276c5ef2d67b9541c30718ff4
Felipe Schmitz <aryastarklinda@outlook.com>
2022-09-21 16:51:40 -0300

    inicio da criação da tela do restaurante

---
+++ app/src/main/java/com/example/restaurantea3/Home_restaurante.java
@@ -0,0 +1,13 @@
+package com.example.restaurantea3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home_restaurante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_restaurante);
+    }
+}
