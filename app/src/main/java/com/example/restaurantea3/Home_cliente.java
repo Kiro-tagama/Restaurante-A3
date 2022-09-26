bbeebbfbb3f0c38c3426eb4fcb879a4fd1aa14ea
Rodrigo Lopes <rodrigo.bleachex@hotmail.com>
2022-09-26 13:54:34 -0300

    up

--- app/src/main/java/com/example/restaurantea3/Home_cliente.java
+++ app/src/main/java/com/example/restaurantea3/Home_cliente.java
@@ -2,22 +2,16 @@ package com.example.restaurantea3;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Button;
+
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import com.example.restaurantea3.databinding.HomeClienteBinding;
-
 public class Home_cliente extends AppCompatActivity {
 
-    String[] array={"coisa-1","coisa-2"};
+    String[] array={"primeiro","segundo","terceiro"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,22 +20,21 @@ public class Home_cliente extends AppCompatActivity {
 
         EditText input_pesquisa = (EditText) findViewById(R.id.input_pesquisa);
         ImageButton bt_perfil = (ImageButton) findViewById(R.id.bt_perfil);
-        View card_restaurante = (View) findViewById(R.id.card_restaurante); // n sei se esse é exatamente uma view
+        //View card_restaurante = (View) findViewById(R.id.card_restaurante); // n sei se esse é exatamente uma view
+
+        RecyclerView lista_restaurantes = (RecyclerView) findViewById(R.id.lista_restaurantes);
 
         bt_perfil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(android.view.View view) {
-
-                System.out.println(array[0]);
-
-                /*
                 Intent intent = new Intent();
                 intent.setClass(getApplicationContext(),Perfil_clienete.class);
                 startActivity(intent);
-                 */
             }
         });
+
     }
+
 
 
 }
